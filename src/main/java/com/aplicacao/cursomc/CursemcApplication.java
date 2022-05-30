9f3840097d34c08da5f8a580384129773698ecd6
Lucas Rodrigues Monteiro <monteiro9961@gmail.com>
2022-05-30 11:26:02 -0300

    Projeto Criado

---
+++ src/main/java/com/aplicacao/cursomc/CursemcApplication.java
@@ -0,0 +1,13 @@
+package com.aplicacao.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursemcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursemcApplication.class, args);
+	}
+
+}
